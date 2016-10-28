35dc601fc848f486d3693f49a1858236c20313c1
nan86150 <861503125@qq.com>
2016-10-28 21:32:03 +0800

    refactor HazeRemovalWidthGuided.py

--- HazeRemovalWidthGuided.py
+++ HazeRemovalWidthGuided.py
@@ -6,48 +6,48 @@ import numpy as np
 from guidedfilter import guidedfilter
 
 
-def ind2sub(array_shape, ind):
-    rows = (ind.astype('int') / array_shape[1])
-    cols = (ind.astype('int') % array_shape[1])  # or numpy.mod(ind.astype('int'), array_shape[1])
-    return (rows, cols)
+class HazeRemoval:
+    def __init__(self, filename, omega = 0.85, r = 40):
+        self.filename = filename
+        self.omega = omega
+        self.r = r
+        self.eps = 10 ** (-3)
+        self.t = 0.1
 
+    def _ind2sub(self, array_shape, ind):
+        rows = (ind.astype('int') / array_shape[1])
+        cols = (ind.astype('int') % array_shape[1]) # or numpy.mod(ind.astype('int'), array_shape[1])
+        return (rows, cols)
 
-def rgb2gray(rgb):
-    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+    def _rgb2gray(self, rgb):
+        return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-imageName = "canon3.bmp"
-# imageName = "Haze.jpg"
-# imageName = "foggy_bench.jpg"
-omega = 0.85
-r = 80
-eps = 10 ** (-3)
-t = 0.1
+    def haze_removal(self):
+        oriImage = np.array(Image.open(self.filename))
+        img = np.array(oriImage).astype(np.double) / 255.0
+        grayImage = self._rgb2gray(img)
 
-oriImage = Image.open('images/' + imageName)
-img = np.array(oriImage).astype(np.double) / 255.0
-# numpy rgb2gray don't equal matlab rgb2gray
-# grayImage = np.array(oriImage.convert('L')).astype(np.double) / 255.0
-grayImage = rgb2gray(img)
+        darkImage = img.min(axis=2)
 
+        (i, j) = self._ind2sub(darkImage.shape, darkImage.argmax())
+        A = img[i, j, :].mean()
+        transmission = 1 - self.omega * darkImage / A
 
-imageSize = img.shape
-darkImage = img.min(axis=2)
+        transmissionFilter = guidedfilter(grayImage, transmission, self.r, self.eps )
+        transmissionFilter[transmissionFilter < self.t] = self.t
 
-# 滤波
-# ordfilt2
-(i, j) = ind2sub(darkImage.shape, darkImage.argmax())
-A = img[i, j, :].mean()
-transmission = 1 - omega * darkImage / A
+        resultImage = np.zeros_like(img)
+        for i in range(3):
+            resultImage[:, :, i] = (img[:, :, i] - A) / transmissionFilter + A
 
-transmissionFilter = guidedfilter(grayImage, transmission, r, eps)
-transmissionFilter[transmissionFilter < t] = t
+        resultImage[resultImage < 0] = 0
+        resultImage[resultImage > 1] = 1
+        result = Image.fromarray((resultImage * 255).astype(np.uint8))
 
-resultImage = np.zeros_like(img)
-for i in range(3):
-    resultImage[:, :, i] = (img[:, :, i] - A) / transmissionFilter + A
+        return result
 
-resultImage[resultImage < 0] = 0
-resultImage[resultImage > 1] = 1
-result = Image.fromarray((resultImage * 255).astype(np.uint8))
-result.show()
-
+if __name__ == '__main__':
+    imageName = "canon3.bmp"
+    hz = HazeRemoval("images/canon3.bmp")
+    result = hz.haze_removal()
+    result.show()
