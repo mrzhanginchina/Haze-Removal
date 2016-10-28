164c357ad9b7d53cc5988bb68d62a2fd3d4d9587
nan86150 <861503125@qq.com>
2016-10-28 21:25:15 +0800

    refactor HazeRemoval.py

--- HazeRemoval.py
+++ HazeRemoval.py
@@ -4,33 +4,29 @@
 from PIL import Image
 import numpy as np
 
-windowSize = 24
-w0 = 0.6
-t0 = 0.1
+def haze_removal(image, windowSize=24, w0=0.6, t0=0.1):
 
-imageName = "tiananmen1.png"
-imageName = "canon3.bmp"
-oriImage = np.array(Image.open('images/' + imageName))
-imageSize = oriImage.shape
+    darkImage = image.min(axis=2)
+    maxDarkChannel = darkImage.max()
+    darkImage = darkImage.astype(np.double)
 
-# TODO
-# 统计<50的像素所占的比例
+    t = 1 - w0 * (darkImage / maxDarkChannel)
+    T = t * 255
+    T.dtype = 'uint8'
 
-darkImage = oriImage.min(axis=2)
-maxDarkChannel = darkImage.max()
-darkImage = darkImage.astype(np.double)
+    t[t < t0] = t0
 
-t = 1 - w0 * (darkImage / maxDarkChannel)
-T = t * 255
-T.dtype = 'uint8'
+    J = image
+    J[:, :, 0] = (image[:, :, 0] - (1 - t) * maxDarkChannel) / t
+    J[:, :, 1] = (image[:, :, 1] - (1 - t) * maxDarkChannel) / t
+    J[:, :, 2] = (image[:, :, 2] - (1 - t) * maxDarkChannel) / t
+    result = Image.fromarray(J)
 
-t[t < t0] = t0
+    return result
 
-# t = np.array([t, t, t])
-J = oriImage
-J[:, :, 0] = (oriImage[:, :, 0] - (1 - t) * maxDarkChannel) / t
-J[:, :, 1] = (oriImage[:, :, 1] - (1 - t) * maxDarkChannel) / t
-J[:, :, 2] = (oriImage[:, :, 2] - (1 - t) * maxDarkChannel) / t
-
-result = Image.fromarray(J)
-result.show()
+if __name__ == '__main__':
+    imageName = "canon3.bmp"
+    image = np.array(Image.open('images/' + imageName))
+    imageSize = image.shape
+    result = haze_removal(image)
+    result.show()
